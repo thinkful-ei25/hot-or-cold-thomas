c1b542bb549a571b2ca720f9391a582c8bc6172d
Thomas Chun <tchun2055@gmail.com>
2018-11-05 15:00:10 -0800

    basic layout completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,8 +6,14 @@ export default function NavBar(props) {
   return (
     <nav className="navigation-bar">
       <ul>
-        <li id="what">WHAT?</li>
-        <li id="new-game">+ NEW GAME</li>
+        {/* <li id="what">What?</li>
+        <li id="new-game">+New Game</li> */}
+        <li>
+          <a href="#" id="what">WHAT?</a>
+        </li>
+        <li>
+          <a href="#" id="new-game">+ NEW GAME</a>
+        </li>
       </ul>
     </nav>
   );
