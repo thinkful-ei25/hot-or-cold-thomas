ad0afd33ec2e08eb2fe7e8855ea7d23035371c96
Thomas Chun <tchun2055@gmail.com>
2018-11-05 14:09:06 -0800

    basic layout completed

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -3,12 +3,12 @@ import './guess-form.css';
 
 export default function GuessForm(props) {
   return (
-    <section className="guess-form">
+    <section className='guess-form'>
       <div className='feedback'>Make Your Guess!</div>
       <form className='guess-input'>
         <input placeholder='Enter Your Guess'></input>
-        <button>Guess</button>
-        <p>Guess #0!</p>
+        <div className='button'><button>Guess</button></div>
+        <p>Guess #<span className='guess-counter'>0</span>!</p>
       </form>
       <div className="guess-history">Guess History</div>
       {/* <GuessHistory /> */}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './components/header';
-import Navbar from './components/navbar';
 import HotOrCold from './components/hotorcoldgame';
 
 
